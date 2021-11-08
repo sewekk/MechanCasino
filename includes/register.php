14af5409b809225e37c8fab468e8c044e782f478
Sewek <seweryq@gmail.com>
2021-11-08 21:01:42 +0100

    Stworzenie skryptu do tworzenia konta

---
+++ includes/register.php
@@ -0,0 +1,40 @@
+<?php
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $email= $_POST['e-mail'];
+        $pwd = $_POST['password'];
+        $pwdRepeat = $_POST['re-password'];
+
+        require_once 'dbh.inc.php';
+        require_once 'functions.inc.php';
+
+        if(emptyInputSignup($name, $email, $pwd, $pwdRepeat)!== false){
+            header('location: ../register.html?error=emptyinput');
+            exit();
+        }
+        if(InvalidUserName($name)!== false){
+            header('location: ../register.html?error=invalidusername');
+            exit();
+        }
+        if(InvalidEmail($email)!== false){
+            header('location: ../register.html?error=invalidemail');
+            exit();
+        }
+        if(pwdMach($pwd, $pwdRepeat)!== false){
+            header('location: ../register.html?error=passworddontmach');
+            exit();
+        }
+        if(usernameExists($conn, $username)!== false){
+            header('location: ../register.html?error=userexists');
+            exit();
+        }
+        createUser($conn, $username, $email, $pwd);
+    }
+
+    else{
+        header('location: ../register.html');
+        exit();
+    }
+
+?>
